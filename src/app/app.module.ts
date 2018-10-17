66c42b8368277a51d2e5f3a2e0550ea992206aba
Murilo Conde <ssk77play@live.com>
2018-10-17 17:34:49 -0300

    add browser animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
